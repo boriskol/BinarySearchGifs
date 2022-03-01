077065e620f5cb79daaf645bdc8cbb9cd7443fed
borna libertines <2532576+boriskol@users.noreply.github.com>
2022-03-01 11:30:03 +0100

    Initial Commit

---
+++ Shared/BinarySearchGifsApp.swift
@@ -0,0 +1,17 @@
+//
+//  BinarySearchGifsApp.swift
+//  Shared
+//
+//  Created by Borna Libertines on 01/03/22.
+//
+
+import SwiftUI
+
+@main
+struct BinarySearchGifsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
